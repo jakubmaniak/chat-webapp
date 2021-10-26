03f661c456eb52dfb292ea0a97dc01cec0e813c2
Jakub Maniak <jakubmaniak99@gmail.com>
2021-10-26 22:20:47 +0200

    Delete unused hooks

--- src/hooks/use-ref-state.js
+++
@@ -1,9 +0,0 @@
-import { useEffect, useRef, useState } from 'react';
-
-function useRefState(initialValue) {
-    const ref = useRef(initialValue);
-    const setter = (newValue) => ref.current = newValue;
-
-    return [ref, setter];
-}
-
