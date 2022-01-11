d9ab51cbd35ce1a733be1526f50e415cb33bbb97
Jakub Maniak <jakubmaniak99@gmail.com>
2022-01-12 00:26:57 +0100

    Improve UI design of login and signup pages

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,17 +1,21 @@
 import { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import api from '../../api';
 import { SessionContext } from '../../contexts/session-context';
+import useToast from '../../hooks/use-toast';
 
-import './login.scss';
+import 'react-toastify/dist/ReactToastify.css';
+import '../../common/auth.scss';
 
 
 function LoginPage() {
     const history = useHistory();
+    const toast = useToast();
     const { session } = useContext(SessionContext);
-    
+
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
@@ -24,19 +28,36 @@ function LoginPage() {
 
     function submitForm(ev) {
         ev.preventDefault();
-        
+
         api.userLogin({ username, password })
-            .then((results) => {
-                if (!results.error) {
+            .then((res) => {
+                if (!res.error) {
                     localStorage.setItem('username', username);
                     history.replace('/messages');
+                }
+                else if (res.code === 'WRONG_PASSWORD') {
+                    toast.error('Podane hasło jest nieprawidłowe');
+                }
+                else if (res.code === 'USER_NOT_FOUND') {
+                    toast.error('Użytkownik o tej nazwie nie istnieje');
+                }
+                else if (res.code === 'PASSWORD_TOO_SHORT') {
+                    toast.error('Podane hasło jest za krótkie');
+                }
+                else {
+                    toast.error('Wystąpił nieokreślony błąd');
                 }
             });
     }
 
     return (
         <div className="page login-page">
+            <ToastContainer limit={4} />
             <form className="login-form" onSubmit={submitForm}>
+                <header>
+                    <div className="logo"></div>
+                    <p>Lingee</p>
+                </header>
                 <input
                     value={username}
                     onChange={(ev) => setUsername(ev.target.value)}
--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -1,19 +1,24 @@
 import { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import api from '../../api';
 import { SessionContext } from '../../contexts/session-context';
 
-import '../login/login.scss';
+import 'react-toastify/dist/ReactToastify.css';
+import '../../common/auth.scss';
+import useToast from '../../hooks/use-toast';
 
 
 function SignupPage() {
     const history = useHistory();
+    const toast = useToast();
     const { session } = useContext(SessionContext);
-    
+
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
 
 
     useEffect(() => {
@@ -24,19 +29,38 @@ function SignupPage() {
 
     function submitForm(ev) {
         ev.preventDefault();
-        
+
+        if (password !== repeatPassword) {
+            toast.error('Podane hasła różnią się między sobą');
+            return;
+        }
+
         api.userSignup({ username, password })
-            .then((results) => {
-                if (!results.error) {
+            .then((res) => {
+                if (!res.error) {
                     localStorage.setItem('username', username);
                     history.replace('/messages');
+                }
+                else if (res.code === 'USER_ALREADY_EXISTS') {
+                    toast.error('Użytkownik o tej nazwie już istnieje');
+                }
+                else if (res.code === 'PASSWORD_TOO_SHORT') {
+                    toast.error('Podane hasło jest za krótkie');
+                }
+                else {
+                    toast.error('Wystąpił nieokreślony błąd');
                 }
             });
     }
 
     return (
-        <div className="page login-page">
-            <form className="login-form" onSubmit={submitForm}>
+        <div className="page signup-page">
+            <ToastContainer limit={4} />
+            <form className="signup-form" onSubmit={submitForm}>
+                <header>
+                    <div className="logo"></div>
+                    <p>Lingee</p>
+                </header>
                 <input
                     value={username}
                     onChange={(ev) => setUsername(ev.target.value)}
@@ -50,6 +74,13 @@ function SignupPage() {
                     onChange={(ev) => setPassword(ev.target.value)}
                     placeholder="Hasło"
                     name="password"
+                    type="password"
+                />
+                <input
+                    value={repeatPassword}
+                    onChange={(ev) => setRepeatPassword(ev.target.value)}
+                    placeholder="Powtórz hasło"
+                    name="repeat-password"
                     type="password"
                 />
                 <button>Zarejestruj</button>
